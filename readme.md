501a319486639134cd0cac49a2c863f4955461ec
LyLy Tran <lylytr.311@gmail.com>
2024-07-07 16:49:52 -0400

    first commit

---
+++ readme.md
@@ -0,0 +1,33 @@
+Host link:
+[ltlely.github.io](https://ltlely.github.io/)
+
+Inspired by:
+https://jung.ninja/
+
+Format for HTML page can be found here: 
+https://www.w3schools.com/html/html_basic.asp
+
+Webpage suitable for laptop and mobile:
+https://www.instructables.com/Make-your-HTML-Website-suitable-for-Mobile-Devices/
+
+Keyframe and style animation:
+https://www.joshwcomeau.com/animation/keyframe-animations/
+https://www.w3schools.com/jsref/dom_obj_style.asp
+
+CSS:
+https://developer.mozilla.org/en-US/docs/Web/HTML/Element/input
+https://tailwindcss.com/docs/flex-direction
+https://www.w3schools.com/html/html_forms.asp
+
+Font style and importing font: 
+https://www.fontspace.com/roboto-remix-font-f26577
+https://stackoverflow.com/questions/18178867/how-can-i-use-googles-roboto-font-on-a-website
+
+PDF display:
+https://blog.openreplay.com/step-by-step--embedding-pdfs-in-html-pages/
+
+Submit javascript:
+https://stackoverflow.com/questions/26631038/how-to-check-if-a-submit-button-has-been-clicked-in-javascript
+
+Z-index:
+https://developer.mozilla.org/en-US/docs/Web/CSS/z-index
---
+++ scripts/opening.js
@@ -0,0 +1,34 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const charImg1 = document.getElementById("char_img1");
+  const bg1 = document.getElementById("bg1");
+  const bg2 = document.getElementById("bg2");
+  const bg3 = document.getElementById("bg3");
+  const moon = document.getElementById("moon");
+  const clickMeText = document.getElementById("clickable-text");
+
+  function handleAnimationEnd() {
+    window.location.href = "index.html";
+  }
+
+  charImg1.addEventListener("click", function () {
+    bg1.classList.add("move-down1");
+    bg2.classList.add("move-down2");
+    bg3.classList.add("move-down3");
+    charImg1.classList.add("move-down4");
+    moon.classList.add("move-out-left");
+    clickMeText.classList.add("move-down5");
+
+    bg1.addEventListener("animationend", handleAnimationEnd, { once: true });
+    bg2.addEventListener("animationend", handleAnimationEnd, { once: true });
+    bg3.addEventListener("animationend", handleAnimationEnd, { once: true });
+    charImg1.addEventListener("animationend", handleAnimationEnd, {
+      once: true,
+    });
+    moon.addEventListener("animationend", handleAnimationEnd, {
+      once: true,
+    });
+    clickMeText.addEventListener("animationend", handleAnimationEnd, {
+      once: true,
+    });
+  });
+});
