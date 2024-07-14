ea6cdee1b18f12dc22859cfd212acc99aec0c7ef
ltlely <lylytr.311@gmail.com>
2024-07-14 16:20:29 -0400

    changes in server

--- server.js
+++ server.js
@@ -1,33 +1,11 @@
-require('dotenv').config();
-const express = require('express');
-const bodyParser = require('body-parser');
 const nodemailer = require('nodemailer');
-const path = require('path');
-const cors = require('cors')
-const app = express();
-const port = 3000;
 
-app.use(cors());
+module.exports = async (req, res) => {
+  if (req.method !== 'POST') {
+    return res.status(405).send('Method Not Allowed');
+  }
 
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
-
-
-app.use(express.static(path.join(__dirname, 'public')));
-
-
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public', 'index.html'));
-});
-
-app.get('/contact', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public', 'contact.html'));
-});
-
-
-app.post('/send-email', (req, res) => {
   const { first_name, last_name, email, phone, message } = req.body;
-
 
   const transporter = nodemailer.createTransport({
     service: 'gmail',
@@ -44,18 +22,11 @@ app.post('/send-email', (req, res) => {
     text: `You have a new contact form submission from ${first_name} ${last_name}.\n\nEmail: ${email}\nPhone: ${phone}\n\nMessage:\n${message}`,
   };
 
-  transporter.sendMail(mailOptions, (error, info) => {
-    if (error) {
-      console.log(error);
-      res.status(500).send('Error sending email');
-    } else {
-      console.log('Email sent: ' + info.response);
-      res.status(200).send('Email sent');
-    }
-  });
-});
-
-
-app.listen(port, () => {
-  console.log(`Server is running on http://localhost:${port}`);
-});
+  try {
+    await transporter.sendMail(mailOptions);
+    res.status(200).send('Email sent');
+  } catch (error) {
+    console.error('Error sending email:', error);
+    res.status(500).send('Error sending email');
+  }
+};
